eb5a8c7e70c48ce0d23ea05ebba515ab3556e0fd
unalterableimpact <100035040+unalterableimpact@users.noreply.github.com>
2022-10-06 15:33:08 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // To identify whether the given number is Even or Odd
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    if(a%2==0){
+        cout<<"Even"<<endl;
+        }
+
+        else{
+            cout<<"Odd"<<endl;
+        }
+        return 0;
+}
